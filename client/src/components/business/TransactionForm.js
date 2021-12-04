f0120122f0d0c61731144b586939229d580dd16a
CKanishka <ckanishka24@gmail.com>
2021-12-04 18:37:41 +0530

    Transaction form reward to be reset

--- client/src/components/business/TransactionForm.js
+++ client/src/components/business/TransactionForm.js
@@ -11,42 +11,57 @@ import {
   Alert,
   Badge,
   Row,
+  message,
 } from 'antd';
 const { Option } = Select;
 
 const TransactionForm = ({ form, customersList = [] }) => {
   const [formValues, setFormValues] = useState({});
+  const [currentCustomerObj, setCurrentCustomerObj] = useState(null);
   const [currentReward, setCurrentReward] = useState(0);
   const [customerId, setCustomerId] = useState(null);
   const [isRedeemed, setIsRedeemed] = useState(false);
+
+  const getCustomerByPhone = (phone) => {
+    return customersList?.find((item) => item.phone === phone);
+  };
   function handleChange(changedValues, allValues) {
     console.log(changedValues);
+    // customer change
     if (changedValues.customerId) {
-      const currentCustomer = customersList?.find((item) => item.phone === changedValues.customerId);
+      const currentCustomer = getCustomerByPhone(changedValues.customerId);
+      setCurrentCustomerObj(currentCustomer);
       setCurrentReward(currentCustomer.reward || 0);
       setCustomerId(currentCustomer?.id);
     }
+    // transaction change
     if (changedValues.transactionAmount !== undefined) {
+      setCurrentReward(currentCustomerObj.reward || 0);
       setIsRedeemed(false);
     }
     setFormValues(allValues);
   }
 
   useEffect(() => {
-      form.setFieldsValue({
-        ...formValues,
-        id: customerId,
-        redeemAmount: 0,
-        redeemPercent: 0,
-        transactionAmount: 0
-      });
-  }, [customerId])
-  
+    form.setFieldsValue({
+      ...formValues,
+      id: customerId,
+      redeemAmount: 0,
+      redeemPercent: 0,
+      transactionAmount: 0,
+    });
+  }, [customerId]);
+
   const onUtiliseRewards = () => {
     const { transactionAmount, redeemPercent } = formValues;
-    if (transactionAmount > 1000 && redeemPercent && !isRedeemed) {
+
+    if (redeemPercent && !isRedeemed) {
       const redeemAmount = (redeemPercent / 100) * currentReward;
       const finalAmount = transactionAmount - redeemAmount;
+      if (transactionAmount < redeemAmount) {
+        message.error('Sorry! Transaction amount should be greater than the amount to be redeemed');
+        return;
+      }
       form.setFieldsValue({
         ...formValues,
         transactionAmount: finalAmount,
@@ -90,7 +105,7 @@ const TransactionForm = ({ form, customersList = [] }) => {
       )}
       <Row align="middle">
         <Form.Item label="Select % of reward points to redeem" name="redeemPercent">
-          <Radio.Group disabled={isRedeemed}>
+          <Radio.Group disabled={isRedeemed || !currentReward}>
             <Radio value="0">0%</Radio>
             <Radio value="25">25%</Radio>
             <Radio value="50">50%</Radio>
